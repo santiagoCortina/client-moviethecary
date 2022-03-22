7aef9e58a089aee70049f0447f0ea5c3a3cb21a0
SantiagoCortina <cortina.zapfe@gmail.com>
2022-03-21 19:16:42 -0600

    Added production server in heroku

--- src/services/api.js
+++ src/services/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const isProduction = process.env.NODE_ENV === "production";
 const baseURL = isProduction
-  ? "http://deylanHeroku.com/api"
+  ? "https://moviethecary-server.herokuapp.com/api"
   : "http://localhost:5005/api";
 
 export const api = axios.create({
